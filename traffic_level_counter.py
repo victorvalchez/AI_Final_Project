f0ffca8d4971be354038713c044ff853d29a42f8
Víctor Valencia <victorvalenciaspam@gmail.com>
2022-05-01 19:23:50 +0200

    counter for changes when E is applied DONE

--- traffic_level_counter.py
+++ traffic_level_counter.py
@@ -19,19 +19,58 @@ with open('Data_no_header.csv', newline='') as csvfile:
         new_list.append(row[0].split(';'))  # Esto crea una lista de lo que hay en cada fila (le pongo que esta separado
         # por ; para que cada vez que vea ; añada un nuevo elemento a la fila
 
+countE = 0
 cLowNToHigh = 0
 cLowEToHigh = 0
 cLowWToHigh = 0
+
+cLowNToLow = 0
+cLowEToLow = 0
+cLowWToLow = 0
+
+cHighNToHigh = 0
+cHighEToHigh = 0
+cHighWToHigh = 0
+
+cHighEToLow = 0
+
 for row in new_list:
     if row[3] == 'E':
-        if row[0] == 'Low' and row[4] == 'High':
+        if row[0] == 'Low' and row[4] == 'High':  # low norht to high north
             cLowNToHigh += 1
-        if row[1] == 'Low' and row[5] == 'High':
+        if row[1] == 'Low' and row[5] == 'High':  # es 0 ya que se abre el semaforo entonces no suele subir
             cLowEToHigh += 1
         if row[2] == 'Low' and row[6] == 'High':
             cLowWToHigh += 1
+
+        if row[0] == 'Low' and row[4] == 'Low':
+            cLowNToLow += 1
+        if row[1] == 'Low' and row[5] == 'Low':
+            cLowEToLow += 1
+        if row[2] == 'Low' and row[6] == 'Low':
+            cLowWToLow += 1
+
+        if row[0] == 'High' and row[4] == 'High':
+            cHighNToHigh += 1
+        if row[1] == 'High' and row[5] == 'High':
+            cHighEToHigh += 1
+        if row[2] == 'High' and row[6] == 'High':
+            cHighWToHigh += 1
+
+        if row[1] == 'High' and row[5] == 'Low':
+            cHighEToLow += 1
+
+        countE += 1
 
 
 print(cLowNToHigh)
 print(cLowEToHigh)
 print(cLowWToHigh)
+print(cLowNToLow)
+print(cLowEToLow)
+print(cLowWToLow)
+print(cHighNToHigh)
+print(cHighEToHigh)
+print(cHighWToHigh)
+print(cHighEToLow)
+print(countE)
